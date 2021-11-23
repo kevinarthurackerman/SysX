cbf754019f6d1df2de776c264ec22b7efc78f7f9
kackerman <kevin.arthur.ackerman@gmail.com>
2021-11-22 18:13:57 -0600

    Adds bulk assembly loader

---
+++ src/Sysx/Reflection/AssemblyLoader.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Sysx.Linq;
+using Assembly = System.Reflection.Assembly;
+
+namespace Sysx.Reflection
+{
+    internal static class AssemblyLoader
+    {
+        internal static IEnumerable<Assembly> Load(Assembly rootAssembly)
+        {
+            return rootAssembly
+                .Descendants(x => x.GetReferencedAssemblies().Select(y => Assembly.Load(y)))
+                .ToArray();
+        }
+    }
+}
