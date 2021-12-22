aa17dfa55da3dafa70f825cb890e8b7ef4b2687c
Kevin Ackerman <kevin.arthur.ackerman@gmail.com>
2021-12-22 16:11:53 -0600

    Adds EnttyFramework project, GUID registration helpers, and test project

---
+++ src/Sysx.EntityFramework.Test/Global.cs
@@ -0,0 +1,9 @@
+﻿global using Microsoft.EntityFrameworkCore;
+global using System;
+global using System.Linq;
+global using System.Threading.Tasks;
+global using Sysx.EntityFramework.Identifiers;
+global using Sysx.EntityFramework.Identifiers.Converters;
+global using Sysx.Identifiers;
+global using Xunit;
+global using Sysx.EntityFramework.Testing;
---
+++ src/Sysx.EntityFramework.Test/Identifiers/Converters/ModelBuilderExtensionsTests.cs
@@ -0,0 +1,51 @@
+﻿namespace Sysx.EntityFramework.Test.Identifiers;
+using Assert = Xunit.Assert;
+
+public class ModelBuilderExtensionsTests
+{
+    [Fact]
+    public async Task Should_Configure_Guid_Column_Types()
+    {
+        using var dbContext = SqlServerTestDbContextActivator.Create<TestDbContext>();
+
+        var connection = dbContext.Database.GetDbConnection();
+
+        await connection.OpenAsync();
+
+        using var command = connection.CreateCommand();
+        command.CommandText = "SELECT [Guid], [BinaryGuid], [StringGuid], [SqlServerGuid] FROM [GuidProperties]";
+
+        using var reader = command.ExecuteReader();
+
+        Assert.Equal("uniqueidentifier", reader.GetDataTypeName(0));
+        Assert.Equal("binary", reader.GetDataTypeName(1));
+        Assert.Equal("char", reader.GetDataTypeName(2));
+        Assert.Equal("uniqueidentifier", reader.GetDataTypeName(3));
+    }
+
+    public class TestDbContext : DbContext
+    {
+        public TestDbContext(DbContextOptions<TestDbContext> options) : base(options) { }
+
+        public DbSet<GuidPropertiesModel> GuidPropertiesModels { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<GuidPropertiesModel>()
+                .ToTable("GuidProperties")
+                .HasKey(x => x.Guid);
+
+            modelBuilder.RegisterGuidConversions();
+        }
+    }
+
+    public class GuidPropertiesModel
+    {
+        public Guid Guid { get; set; }
+        public BinaryGuid BinaryGuid { get; set; }
+        public StringGuid StringGuid { get; set; }
+        public SqlServerGuid SqlServerGuid { get; set; }
+    }
+}
