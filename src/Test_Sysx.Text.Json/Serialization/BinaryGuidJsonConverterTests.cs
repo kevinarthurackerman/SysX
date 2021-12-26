e887e3bdaeccb8bd54a9b5ddba31ffbcdb3fd648
Kevin Ackerman <kevin.arthur.ackerman@gmail.com>
2021-12-26 13:02:28 -0600

    Adds tests for nullable sequential guid json serialization

--- src/Test_Sysx.Text.Json/Serialization/BinaryGuidJsonConverterTests.cs
+++ src/Test_Sysx.Text.Json/Serialization/BinaryGuidJsonConverterTests.cs
@@ -19,4 +19,38 @@ public class BinaryGuidJsonConverterTests
 
         Assert.Equal(value, deserializedValue);
     }
+
+    [Fact]
+    public void Should_Persist_Nullable()
+    {
+        var options = new JsonSerializerOptions();
+        options.Converters.AddSequentialGuidJsonConverters();
+
+        var value = (BinaryGuid?)BinaryGuid.NewGuid();
+
+        var json = JsonSerializer.Serialize(value, options);
+
+        Assert.Equal($"\"{value}\"", json);
+
+        var deserializedValue = JsonSerializer.Deserialize<BinaryGuid?>(json, options);
+
+        Assert.Equal(value, deserializedValue);
+    }
+
+    [Fact]
+    public void Should_Persist_Null()
+    {
+        var options = new JsonSerializerOptions();
+        options.Converters.AddSequentialGuidJsonConverters();
+
+        var value = (BinaryGuid?)null;
+
+        var json = JsonSerializer.Serialize(value, options);
+
+        Assert.Equal("null", json);
+
+        var deserializedValue = JsonSerializer.Deserialize<BinaryGuid?>(json, options);
+
+        Assert.Equal(value, deserializedValue);
+    }
 }
