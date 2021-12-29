b34e63df6b2840bafbc25e68147a0c031623f898
Kevin Ackerman <kevin.arthur.ackerman@gmail.com>
2021-12-29 17:59:52 -0600

    Removes unnecessary single guid type extensions

--- src/Sysx.Text.Json/IListOfJsonConverterExtensions.cs
+++ src/Sysx.Text.Json/IListOfJsonConverterExtensions.cs
@@ -4,17 +4,8 @@ public static class IListOfJsonConverterExtensions
 {
     public static void AddSequentialGuids(this IList<JsonConverter> jsonConverters)
     {
-        jsonConverters.AddBinaryGuids();
-        jsonConverters.AddSqlServerGuids();
-        jsonConverters.AddStringGuids();
-    }
-
-    public static void AddBinaryGuids(this IList<JsonConverter> jsonConverters) =>
         jsonConverters.Add(BinaryGuidJsonConverter.Instance);
-
-    public static void AddSqlServerGuids(this IList<JsonConverter> jsonConverters) =>
         jsonConverters.Add(SqlServerGuidJsonConverter.Instance);
-
-    public static void AddStringGuids(this IList<JsonConverter> jsonConverters) =>
         jsonConverters.Add(StringGuidJsonConverter.Instance);
+    }
 }
