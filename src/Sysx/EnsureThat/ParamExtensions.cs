44537c3b860ccdf27b082dbb668a633075c176bd
Kevin Ackerman <kevin.arthur.ackerman@gmail.com>
2022-01-11 09:32:22 -0600

    Adds EnsureThat helper extensions for collections

--- src/Sysx/EnsureThat/ParamExtensions.cs
+++ src/Sysx/EnsureThat/ParamExtensions.cs
@@ -24,7 +24,58 @@ public static class ParamExtensions
             message => new ArgumentNullException(param.Name, message));
     }
 
+    public static void DoesNotContain<T>(this Param<IEnumerable<T>> param, T value)
+    {
+        if (value == null)
+            if (param.Value.All(x => x != null)) return;
+        else if (param.Value.Where(x => x != null).All(x => !x!.Equals(value))) return;
+
+        var commaSeparatedValues = string.Join(", ", param.Value);
+
+        throw ThrowException(
+            param,
+            $"Value '{param.Name}' '[{commaSeparatedValues}]' must not contain a value of {value}.",
+            message => new ArgumentException(param.Name, message));
+    }
+
+    public static void DoesNotContain(this StringParam param, char value)
+    {
+        if (param.Value.All(x => x != value)) return;
+
+        throw ThrowException(
+            param,
+            $"Value '{param.Name}' '{param.Value}' must not contain char {value}.",
+            message => new ArgumentException(param.Name, message));
+    }
+
+    public static void IsNotContainedIn<T>(this Param<T> param, IEnumerable<T> value)
+    {
+        if (param.Value == null)
+            if (value.All(x => x != null)) return;
+            else if (value.Where(x => x != null).All(x => !x!.Equals(param.Value))) return;
+
+        var commaSeparatedValues = string.Join(", ", value);
+
+        throw ThrowException(
+            param,
+            $"Value '{param.Name}' '{param.Value}' must not be contained in '[{commaSeparatedValues}]'.",
+            message => new ArgumentException(param.Name, message));
+    }
+
     private static Exception ThrowException<T>(Param<T> param, string defaultMessage, DefaultExceptionFactory defaultExceptionFactory)
+    {
+        var options = param.OptsFn?.Invoke(new EnsureOptions()) ?? new EnsureOptions();
+
+        var message = options.CustomMessage ?? defaultMessage;
+
+        var exception = options.CustomExceptionFactory?.Invoke(message, param.Name)
+            ?? options.CustomException
+            ?? defaultExceptionFactory.Invoke(message);
+
+        return exception;
+    }
+
+    private static Exception ThrowException(StringParam param, string defaultMessage, DefaultExceptionFactory defaultExceptionFactory)
     {
         var options = param.OptsFn?.Invoke(new EnsureOptions()) ?? new EnsureOptions();
 
