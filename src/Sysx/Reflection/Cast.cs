ec9dbae81f1253456404a3af3dff8ee0e74a95cb
Kevin Ackerman <kevin.arthur.ackerman@gmail.com>
2022-01-11 19:30:15 -0600

    Adds summaries to Cast class

--- src/Sysx/Reflection/Cast.cs
+++ src/Sysx/Reflection/Cast.cs
@@ -1,5 +1,9 @@
 ﻿namespace Sysx.Reflection;
 
+/// <summary>
+/// Used to cast a value from one type to another.
+/// Supports casting to the same type, from an inherited type, to an implemented interface, IConvertible casts, explicitly, and implicitly.
+/// </summary>
 public static class Cast<TFrom, TTo>
 {
     private static readonly bool canCast;
@@ -44,39 +48,56 @@ public static class Cast<TFrom, TTo>
         }
     }
 
+    /// <summary>
+    /// Checks if a cast can be made.
+    /// </summary>
     public static bool CanCast() => canCast;
 
+    /// <summary>
+    /// Casts from one type to another.
+    /// Supports casting to the same type, from an inherited type, to an implemented interface, IConvertible casts, explicitly, and implicitly.
+    /// </summary>
     public static TTo? Value(TFrom? from) => castValue(from);
 }
 
+/// <inheritdoc cref="Cast{TFrom, TTo}" />
 public static class Cast<TFrom>
 {
     private static readonly ConcurrentDictionary<Type, bool> canCastCache = new();
     private static readonly ConcurrentDictionary<Type, MethodInfo> castValueCache = new();
 
+    /// <inheritdoc cref="Cast{TFrom, TTo}.CanCast" />
     public static bool CanCast<TTo>() => CanCast(typeof(TTo));
 
+    /// <inheritdoc cref="Cast{TFrom, TTo}.CanCast" />
     public static bool CanCast(Type to) => canCastCache.GetOrAdd(to, toType =>
         (bool)typeof(Cast<,>).MakeGenericType(typeof(TFrom), toType).GetMethod(nameof(CanCast))!.Invoke(null, null)!);
 
+    /// <inheritdoc cref="Cast{TFrom, TTo}.Value" />
     public static TTo? Value<TTo>(TFrom? value) => (TTo?)Value(typeof(TTo), value);
 
+    /// <inheritdoc cref="Cast{TFrom, TTo}.Value" />
     public static object? Value(Type to, object? value) => castValueCache.GetOrAdd(to, toType =>
         typeof(Cast<,>).MakeGenericType(typeof(TFrom), toType).GetMethod(nameof(Value))!).Invoke(null, new[] { value });
 }
 
+/// <inheritdoc cref="Cast{TFrom, TTo}" />
 public static class Cast
 {
     private static readonly ConcurrentDictionary<CastCacheKey, bool> canCastCache = new();
     private static readonly ConcurrentDictionary<CastCacheKey, MethodInfo> castValueCache = new();
 
+    /// <inheritdoc cref="Cast{TFrom, TTo}.CanCast" />
     public static bool CanCast<TFrom, TTo>() => CanCast(typeof(TFrom), typeof(TTo));
 
+    /// <inheritdoc cref="Cast{TFrom, TTo}.CanCast" />
     public static bool CanCast(Type from, Type to) => canCastCache.GetOrAdd(new CastCacheKey(from, to), x =>
         (bool)typeof(Cast<,>).MakeGenericType(x.From, x.To).GetMethod(nameof(CanCast))!.Invoke(null, null)!);
 
+    /// <inheritdoc cref="Cast{TFrom, TTo}.Value" />
     public static TTo? Value<TFrom, TTo>(TFrom? value) => (TTo?)Value(typeof(TFrom), typeof(TTo), value);
 
+    /// <inheritdoc cref="Cast{TFrom, TTo}.Value" />
     public static object? Value(Type from, Type to, object? value) => castValueCache.GetOrAdd(new CastCacheKey(from, to), x =>
         typeof(Cast<,>).MakeGenericType(x.From, x.To).GetMethod(nameof(Value))!).Invoke(null, new[] { value });
 
