9d0dec819ebfb61a690e90364974faf538cdb1ec
Kevin Ackerman <kevin.arthur.ackerman@gmail.com>
2022-07-29 23:28:50 -0600

    Simplifies per code analysis suggestions.

--- src/Test_SysX/Enums/BaseEnumeration/Color.cs
+++ src/Test_SysX/Enums/BaseEnumeration/Color.cs
@@ -2,8 +2,8 @@
 
 public class Color : BaseEnumeration<Color, int>
 {
-	public static readonly Color Red = new Color(1, "Red");
-	public static readonly Color Blue = new Color(2, "Blue");
+	public static readonly Color Red = new (1, "Red");
+	public static readonly Color Blue = new (2, "Blue");
 
 	private Color(int value, string displayName) : base(value, displayName)
 	{
--- src/Test_SysX/Enums/BaseEnumeration/State.cs
+++ src/Test_SysX/Enums/BaseEnumeration/State.cs
@@ -2,11 +2,11 @@
 
 public class State : BaseEnumeration<State, int>
 {
-	public static readonly State Off = new State(1, "Off");
-	public static readonly State Starting = new State(2, "Starting");
-	public static readonly State Waiting = new State(3, "Waiting");
-	public static readonly State Busy = new State(4, "Busy");
-	public static readonly State Stopping = new State(5, "Stopping");
+	public static readonly State Off = new (1, "Off");
+	public static readonly State Starting = new (2, "Starting");
+	public static readonly State Waiting = new (3, "Waiting");
+	public static readonly State Busy = new (4, "Busy");
+	public static readonly State Stopping = new (5, "Stopping");
 
 	private State(int value, string displayName) : base(value, displayName)
 	{
